38b59c7cb1736eba24f08fab73753e07f262927f
IvoArasin <86802959+IvoArasin@users.noreply.github.com>
2023-03-07 11:28:58 +0100

    Some preprocessing functions
    I added a set of function for the preprocessing stage such as:
    - KNNImputer for zipcode
    - One-One encoder
    - A "binner" to reduce the number of levels of some categorical variables (e.g. "property_type")
    - a function to calculate the date-difference between a date and today as a number in days
    - a function to easily impute with the mean (or median) throughout multiple columns
    - two PCA transformations

--- Assignment1/assignment1_preprocessing.py
+++ Assignment1/assignment1_preprocessing.py
@@ -40,5 +40,96 @@ train_df['property_last_updated_dt'] = train_df['property_scraped_at'] + train_d
 train_df['daypassed_since_lastreview'] = train_df['property_scraped_at'] - train_df['reviews_last']
 train_df['daypassed_since_firstreview'] = train_df['property_scraped_at'] - train_df['reviews_first']
 
+
+
+# some function for preprocessing
+def medianImputer(dataset, columns):
+    for i in columns:
+        dataset[str(i)] = dataset[str(i)].fillna(dataset[str(i)].median())
+    return dataset
+
+# PCA transformation of property features as seen in the array below
+pcas = train_df[['property_max_guests', 'property_bathrooms', 'property_bathrooms', 'property_bedrooms']]
+train_df.drop(['property_max_guests', 'property_bathrooms', 'property_bathrooms', 'property_bedrooms'], axis=1, inplace=True)
+medianImputer(pcas, ['property_max_guests', 'property_bathrooms', 'property_bathrooms', 'property_bedrooms'])
+pcas = (pcas - pcas.mean())/np.std(pcas)
+pca_model = sklearn.decomposition.PCA()
+pca_model.fit(pcas)
+flat_features = pd.DataFrame(pca_model.transform(pcas))
+train_df['flatFeature1'] = flat_features.iloc[:,0]
+train_df['flatFeature2'] = flat_features.iloc[:,1]
+train_df['flatFeature3'] = flat_features.iloc[:,2]
+#print(sum(pca_model.explained_variance_[0:2])/sum(pca_model.explained_variance_)) #0.91
+
+# PCA transformation for another set of highly correlated variables as seen below
+pcas2 = pd.DataFrame(train_df[['reviews_num','reviews_rating','reviews_acc','reviews_cleanliness','reviews_checkin','reviews_communication','reviews_location','reviews_value','reviews_per_month']])
+train_df.drop(['reviews_num','reviews_rating','reviews_acc','reviews_cleanliness','reviews_checkin','reviews_communication','reviews_location','reviews_value','reviews_per_month'], axis=1, inplace=True)
+medianImputer(pcas2, ['reviews_num','reviews_rating','reviews_acc','reviews_cleanliness','reviews_checkin','reviews_communication','reviews_location','reviews_value','reviews_per_month'])
+pcas2 = (pcas2 - pcas2.mean())/np.std(pcas2)
+pca2_model = sklearn.decomposition.PCA()
+pca2_model.fit(pcas2)
+review_features = pd.DataFrame(pca2_model.transform(pcas2))
+train_df['reviews1'] = review_features.iloc[:,0]
+train_df['reviews2'] = review_features.iloc[:,1]
+train_df['reviews3'] = review_features.iloc[:,2]
+train_df['reviews4'] = review_features.iloc[:,3]
+#print(sum(pca2_model.explained_variance_[0:3])/sum(pca2_model.explained_variance_))
+
+
+# function to impute missing values with based on KNN. e.g. impute zipcode based on latitude and longitude would look like this:
+# KNNimpute(train_set, 'property_zipcode', ['property_lat', 'property_lon'], 3)
+# the function directly replaces the NAs in the original dataframe, no need to append a new column with imputed values and delete the old one etc.
+def KNNimpute(dataset, imputeValue, imputeWith, nr_of_neighbours):
+    knn_set = pd.DataFrame()
+    for i in imputeWith:
+        knn_set[str(i)] = (dataset[str(i)] - dataset[str(i)].mean())/np.std(dataset[str(i)])
+    knn_set[str(imputeValue)] = dataset[str(imputeValue)]
+    knn_set_clean = knn_set[~knn_set.isna().any(axis=1)] # ~ operator negates if it is NA, thus returns only complete rows without NA
+    knn_imputer = KNeighborsClassifier(n_neighbors=nr_of_neighbours)
+    knn_imputer.fit(knn_set_clean.iloc[:,:-1].values, knn_set_clean.iloc[:,-1].values)
+    dataset[str(imputeValue)] = knn_set.apply(lambda row: knn_imputer.predict([row.iloc[:-1]])[0] if pd.isna(row.iloc[-1]) else row.iloc[-1], axis=1)
+
+KNNimpute(train_df, 'property_zipcode', ['property_lat', 'property_lon'], 3)
+
+# Create bins for property_type corresponding to target_variance
+def prop_type_bins(t):
+    if t in ['Apartment']:
+        return 'apartment'
+    if t in ['House']:
+        return 'house'
+    if t in ['Loft', 'Bed & Breakfast']:
+        return 'loft_and_bdnbrfst'
+    if t in ['Townhouse', 'Guesthouse', 'Condominium', 'Other']:
+        return 'town_guest_condo_other'
+    else:
+        return 'other'
+
+train_df['property_feature_type'] = train_df.apply(lambda x: prop_type_bins(x['property_type']),axis=1)
+train_df.drop(['property_type'], axis=1, inplace=True)
+
+# Change "host_since" to number of days since host has been active up until today
+def datediff_in_days_to_today(d):
+    try:
+        return (datetime.datetime.utcnow() - datetime.datetime.strptime(str(d), '%Y-%m-%d')).days
+    except (ValueError, TypeError):
+        return 0
+
+train_df['host_since_in_days'] = train_df.apply(lambda x: datediff_in_days_to_today(x['host_since']) , axis=1)
+train_df.drop(['host_since'], axis=1, inplace=True)
+
+# Hot-One encode various columns
+def hotOneEncode(dataset, columns):
+    dummies = pd.DataFrame()
+    encoder = sklearn.preprocessing.OneHotEncoder(handle_unknown='ignore')
+    for i in columns:
+        encoder_df = pd.DataFrame(encoder.fit_transform(dataset[[str(i)]]).toarray())
+        names = []
+        for j in range(len(encoder_df.columns)-1):
+            dummies[str(i+str(j))] = encoder_df.iloc[:, j]
+    return dummies
+dums = hotOneEncode(train_df, ['property_feature_type', 'property_room_type', 'property_bed_type', 'property_scraped_at', 'host_response_time', 'booking_cancel_policy'])
+train_df.drop(['property_feature_type', 'property_room_type', 'property_bed_type', 'property_scraped_at', 'host_response_time', 'booking_cancel_policy'], axis=1, inplace=True)
+train_df = pd.concat([train_df, dums], axis=1)
+
 ### just import the module to access train df
 ### from assignment1_preprocessing import train_df
