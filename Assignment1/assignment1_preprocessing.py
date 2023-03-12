7fc745bb17886282007e5333f656fcc3f16fecfa
dorabijvoet <dora.bijvoet@hotmail.fr>
2023-03-12 13:48:30 +0100

    Formatting

--- Assignment1/assignment1_preprocessing.py
+++ Assignment1/assignment1_preprocessing.py
@@ -6,18 +6,20 @@ date_columns = ['property_scraped_at', 'host_since', 'reviews_first', 'reviews_l
 train_df = pd.read_csv("train.csv", parse_dates=date_columns)
 test_df = pd.read_csv("test.csv", parse_dates=date_columns)
 
+
 def string_to_int(input_var):
     return_number = 0
     if input_var[0] == 'a':
         return_number = 1
     else:
-        return_number = int(input_var.split(" ",1)[0])    
+        return_number = int(input_var.split(" ", 1)[0])
     return return_number
 
+
 last_updated_text = train_df.property_last_updated.unique()
-#last_updated_text = np.array_str(last_updated_text)
+# last_updated_text = np.array_str(last_updated_text)
 my_list = []
-for mytext in last_updated_text: 
+for mytext in last_updated_text:
     if "week" in mytext:
         xyz = string_to_int(mytext)
         datetime_date = timedelta(weeks=-xyz)
@@ -28,13 +30,13 @@ for mytext in last_updated_text:
         xyz = string_to_int(mytext)
         datetime_date = timedelta(days=-xyz)
     elif mytext == "yesterday":
-        datetime_date = timedelta(days = -1)
+        datetime_date = timedelta(days=-1)
     else:
         datetime_date = timedelta()
     my_list.append([mytext, datetime_date])
 
 train_df['property_last_updated'] = train_df['property_last_updated'].apply(lambda x: dict(my_list)[x])
-#train_df2['property_scraped_at'] = pd.to_datetime(train_df2['property_scraped_at'])
+# train_df2['property_scraped_at'] = pd.to_datetime(train_df2['property_scraped_at'])
 train_df['property_last_updated_dt'] = train_df['property_scraped_at'] + train_df['property_last_updated']
 
 train_df['daypassed_since_lastreview'] = train_df['property_scraped_at'] - train_df['reviews_last']
